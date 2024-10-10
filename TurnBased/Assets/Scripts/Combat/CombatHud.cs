2411b329d5ac52ee2892afea42c9680f98f8032f
mateusdaemon <mateusjdm@gmail.com>
2024-10-10 01:37:23 -0300

    created combat hud and combat player

--- TurnBased/Assets/Scripts/Combat/CombatHud.cs
+++ TurnBased/Assets/Scripts/Combat/CombatHud.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CombatHud : MonoBehaviour
 {
+    [Header("Life Bars")]
+    public Image playerLife;
+    public Image enemyLife;
+
+    [Header("Skill buttons")]
+    public Button loyalt;
+    public Button wisdom;
+    public Button spirit;
+    public Button expertise;
+
+    [Header("Resistance Texts")]
+    public TextMeshProUGUI loyaltRes;
+    public TextMeshProUGUI wisdomRes;
+    public TextMeshProUGUI spiritRes;
+    public TextMeshProUGUI expertiseRes;
     // Start is called before the first frame update
     void Start()
     {
